bf8792113f2ae97ab54ca4875075fe3e2fbc6273
Chris <chrishethertonweb@gmail.com>
2022-11-11 16:12:20 +0100

    added link to painting page

--- art-gallery-kamris/src/components/Home.js
+++ art-gallery-kamris/src/components/Home.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { data } from "../data/DataBase";
+import { Link } from "react-router-dom";
 
 function Home() {
 
     const [transition,setTransition] = useState(data)
     
     function onMouseHandler(e){
-       console.log('CLICK',e.target.id);
       let imageHovered = transition.filter(item => item.id === e.target.id)
         setTransition(imageHovered)
     }
@@ -19,33 +19,40 @@ function Home() {
     if(transition.length > 1 ){
          allImages = transition.map((item) => {
             return  (
-             <div key={item.id} className="single-image-container"
-              >
-                 <img 
-                 className='image__paintings'
-                 src={item.image}
-                 id={item.id}
-                 onMouseOver={(e)=>onMouseHandler(e)}
-                 onMouseOut={offMouseHandler}
-                 alt=""
-                 />
-              </div>
+                <Link className="" to={`/about/${item.title}`}>
+                <div key={item.id} className="single-image-container"
+                >
+                   <img 
+                   className='image__paintings'
+                   src={item.image}
+                   id={item.id}
+                //    onMouseOver={(e)=>onMouseHandler(e)}
+                //    onMouseOut={offMouseHandler}
+                   alt=""
+                   />
+                </div>
+                </Link>
             )    
          })
     } else {
         allImages = transition.map((item) => {
             return  (
              <div key={item.id} className="single-image-container"
+             style={{
+                // width:'100vh',
+                // opacity: '.7',
+                // background:'rgb(50, 41, 41, .7)'
+             }}
               >
-                <span><p>{item.title}</p><p>{item.year}</p></span>
                  <img 
-                 className='image-mouse__off'
+                 className='image-mouse__off flex-image'
                  src={item.image}
                  id={item.id}
-                 onMouseOver={(e)=>onMouseHandler(e)}
+                 onMouseEnter={(e)=>onMouseHandler(e)}
                  onMouseOut={offMouseHandler}
                  alt=""
                  />
+                <span><p>{item.title}</p><p>{item.year}</p></span>
               </div>
             )    
          })
