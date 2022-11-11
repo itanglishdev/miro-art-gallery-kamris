457c994f4535b6a16fb674055d7d6840ee0f497b
Chris <chrishethertonweb@gmail.com>
2022-11-11 15:20:53 +0100

    onHover on Main page fixed

--- art-gallery-kamris/src/components/Home.js
+++ art-gallery-kamris/src/components/Home.js
@@ -1,43 +1,62 @@
 import { useState } from "react";
 import { data } from "../data/DataBase";
+
 function Home() {
 
-    const [transition,setTransition] = useState(false)
+    const [transition,setTransition] = useState(data)
     
-
     function onMouseHandler(e){
-        if (e.id === data.id) {
-            setTransition(true)
-        } return
-        
+       console.log('CLICK',e.target.id);
+      let imageHovered = transition.filter(item => item.id === e.target.id)
+        setTransition(imageHovered)
     }
     function offMouseHandler(){
-        setTransition(false)
+         setTransition(data)
+    }
+
+    let allImages =[]
+
+    if(transition.length > 1 ){
+         allImages = transition.map((item) => {
+            return  (
+             <div key={item.id} className="single-image-container"
+              >
+                 <img 
+                 className='image__paintings'
+                 src={item.image}
+                 id={item.id}
+                 onMouseOver={(e)=>onMouseHandler(e)}
+                 onMouseOut={offMouseHandler}
+                 alt=""
+                 />
+              </div>
+            )    
+         })
+    } else {
+        allImages = transition.map((item) => {
+            return  (
+             <div key={item.id} className="single-image-container"
+              >
+                <span><p>{item.title}</p><p>{item.year}</p></span>
+                 <img 
+                 className='image-mouse__off'
+                 src={item.image}
+                 id={item.id}
+                 onMouseOver={(e)=>onMouseHandler(e)}
+                 onMouseOut={offMouseHandler}
+                 alt=""
+                 />
+              </div>
+            )    
+         })
     }
 
     return ( 
-
         <div  className="images-container">
-            {
-                data.map(item => {
-                    return (
-                     <div className="single-image-container"
-                     key={item.id}>
-                        <img 
-                        className={`${transition && 'image-mouse__off'} image__paintings`}
-                        src={item.image}
-                        onMouseOver={(e)=>onMouseHandler(e.target.value)}
-                        onMouseOut={offMouseHandler}
-                        alt=""
-                        />
-                        {transition && <span><p>{item.title}</p><p>{item.year}</p></span>}
-                     </div>
-                    )        
-                    // return <img  key={item.id} className="home-page-image"  src={item.image} alt=""  />    
-                })
-            }
+            {allImages}
         </div>
      );
+        
 }
 
 export default Home;
