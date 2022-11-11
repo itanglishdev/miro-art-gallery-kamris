8e4b6b60b9a31d36a6f48a78604fe16fb8b5320a
Chris <chrishethertonweb@gmail.com>
2022-11-11 12:05:30 +0100

    contact form set up

--- art-gallery-kamris/src/components/Contact.js
+++ art-gallery-kamris/src/components/Contact.js
@@ -1,6 +1,64 @@
+import React, { useRef} from 'react'
+
 function Contact() {
+
+    const nameRef = useRef('')
+    const emailRef = useRef('')
+    const messageRef = useRef('')
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        const data = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            message: messageRef.current.value
+        }
+        alert(`thank you ${data.name}, your message ${data.message} has been sent and we will reply asap at ${data.email}`)
+    }
+
+
     return ( 
-        <div>CONTACT US HERE</div>
+        <div className='contact-container'>
+            <p>Fill up the form</p>
+            <form action="">
+                <label htmlFor="name">
+                    <input
+                    ref={nameRef} 
+                    name="name"
+                    tabIndex='1'
+                    type="text" 
+                    placeholder="your name" 
+                    required/>
+                </label>
+                <label htmlFor="email">
+                    <input 
+                    ref={emailRef} 
+                    name="email"
+                    tabIndex='2'
+                    type="email" 
+                    placeholder="youremail@example.com" 
+                    required/>
+                </label>
+                <label htmlFor="message">
+                    <input 
+                    ref={messageRef} 
+                    name="message"
+                    tabIndex='3'
+                    type="comment" 
+                    placeholder="your message..." 
+                    required/>
+                </label>
+                <button
+                onClick={handleSubmit}
+                type="submit"
+                tabIndex='4'
+                >
+                Send
+                </button>
+
+            </form>
+        </div>
      );
 }
 
